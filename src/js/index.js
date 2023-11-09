a89c03849911bad66a09cab4c3bbd5f7c8523e47
Lena <elenazyamileva@gmail.com>
2023-11-09 20:59:09 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import {
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 import '../css/styles.css';
-import Notiflix from 'notiflix';
+import Notiflix, { Loading } from 'notiflix';
 
 const ref = {
   selectData: document.querySelector('.breed-select'),
@@ -17,53 +17,53 @@ const ref = {
 
 const { selectData, catInfo, loader, error } = ref;
 const arrayBreeds = [];
-
-selectData.classList.add('visually-hidden');
-error.classList.add('visually-hidden');
-catInfo.classList.add('visually-hidden');
+let flag = 0;
 
 fetchBreeds()
   .then(data => {
     data.forEach(element => {
       arrayBreeds.push({ text: element.name, value: element.id });
     });
-
-    let slim = new SlimSelect({
+    new SlimSelect({
       select: selectData,
       data: arrayBreeds,
     });
-    selectData.classList.remove('visually-hidden');
   })
   .catch(onError);
 
 selectData.addEventListener('change', selectBreed);
 
 function selectBreed(event) {
-  loader.classList.replace('visually-hidden', 'loader');
-  selectData.classList.add('visually-hidden');
-  catInfo.classList.add('visually-hidden');
+  if (flag === 1) {
+    loader.classList.replace('visually-hidden', 'loader');
+    catInfo.classList.add('visually-hidden');
 
-  const { currentTarget } = event;
-  fetchCatByBreed(currentTarget.value)
-    .then(data => {
-      const { id, url } = data[0];
-      fetchCatDiscriptionByBreed(id)
-        .then(element => {
-          loader.classList.replace('loader', 'visually-hidden');
-          selectData.classList.remove('visually-hidden');
-          const { temperament, name, description } = element.breeds[0];
-          catInfo.innerHTML = `<div><img src="${url}" alt="${name}" width="400"/></div><div><h1>${name}</h1><p>${description}</p><p><b>Temperament:</b> ${temperament}</p></div>`;
-          catInfo.classList.remove('visually-hidden');
-        })
-        .catch(onError);
-    })
-    .catch(onError);
+    const { currentTarget } = event;
+    fetchCatByBreed(currentTarget.value)
+      .then(data => {
+        const { id, url } = data[0];
+        fetchCatDiscriptionByBreed(id)
+          .then(element => {
+            loader.classList.replace('loader', 'visually-hidden');
+            const { temperament, name, description } = element.breeds[0];
+            catInfo.innerHTML = `<div><img src="${url}" alt="${name}" width="400"/></div><div><h1>${name}</h1><p>${description}</p><p><b>Temperament:</b> ${temperament}</p></div>`;
+            loader.classList.replace('loader', 'visually-hidden');
+            catInfo.classList.remove('visually-hidden');
+          })
+          .catch(onError);
+      })
+      .catch(onError);
+  } else {
+    loader.classList.replace('loader', 'visually-hidden');
+    flag = 1;
+    return;
+  }
 }
 
 function onError(error) {
-  selectData.classList.remove('visually-hidden');
+  selectData.classList.add('visually-hidden');
   loader.classList.replace('loader', 'visually-hidden');
-
+  Loading.remove();
   Notiflix.Notify.failure(
     'Oops! Something went wrong! Try reloading the page!',
     {
